287757b600e26247987f5a960b1978206f844a8f
rakosg <rakog@oregonstate.edu>
2021-10-03 14:27:45 -0700

    discovered .times and .each_with_index which helped me finish this quicker than expected

--- main.rb
+++ main.rb
@@ -8,3 +8,17 @@ class Player
         @score = score
     end
 end
+
+50.times do |i| 
+    players << Player.new("Player #{i}", rand(10..300))
+end 
+
+players.each_with_index do |player, i|
+    puts "Ready Player #{player.name}! Score #{player.score}"
+end
+
+# "puts players" alone prints extra stuff that is not needed 
+# so this stops that from printing.
+while players < 50
+    puts players
+end
